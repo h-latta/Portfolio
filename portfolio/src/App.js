c7f92021e787179d742bb0d86232d1cea08fafc0
h-latta <h.latta@yahoo.com>
2022-05-16 16:54:59 -0500

    Installed Bootstrap and Added Headers to App File

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Hi, my name is Harrison Latta.</h1>
+      <h1>Welcome to my portfolio.</h1>
     </div>
   );
 }
