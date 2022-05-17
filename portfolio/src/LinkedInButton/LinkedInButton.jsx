0e59209dc3fd7865e044e82a7420eb33cb241ded
h-latta <h.latta@yahoo.com>
2022-05-17 11:39:04 -0500

    Inverted Icon Colors

--- portfolio/src/LinkedInButton/LinkedInButton.jsx
+++ portfolio/src/LinkedInButton/LinkedInButton.jsx
@@ -2,7 +2,7 @@ import './LinkedInButton.css'
 import LinkedInIcon from "../Images/LinkedInIcon.png"
 const GitHubButton = (props) => {
     return ( 
-        <a href="https://www.linkedin.com/in/harrison-latta-90860a219/"><img className='link' src={LinkedInIcon} alt="LinkedIn"/></a>
+        <a href="https://www.linkedin.com/in/harrison-latta-90860a219/"><img className='links' src={LinkedInIcon} alt="LinkedIn"/></a>
      );
 }
  
