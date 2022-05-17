4fa0d18e2f453e10a3767f79070b9f6dfebdab1c
h-latta <h.latta@yahoo.com>
2022-05-17 09:12:43 -0500

    Added Background Image to App, Created Project Files to Add GitHub Button

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 
 function App() {
   return (
+    <body>
     <div className="App">
       <h1>Hi, my name is Harrison Latta.</h1>
       <h1>Welcome to my portfolio.</h1>
     </div>
+    </body>
   );
 }
 
