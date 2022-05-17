c664cfa5b022513593039763c04086296fa7e230
h-latta <h.latta@yahoo.com>
2022-05-17 10:36:25 -0500

    Added Headings and Style to Home Page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -6,11 +6,16 @@ function App() {
   return (
     <body className='App'>
     <div>
-      <h1>Hi, my name is Harrison Latta.</h1>
-      <h1>Welcome to my portfolio.</h1>
+      <h1 className='heading-top'>Hi, my name is Harrison Latta.</h1>
+      <h1 className='heading-bottom'>Welcome to my portfolio.</h1>
     </div>
+    <div>
+      <h2 className='link-style'>Important Links</h2>
     <GitHubButton />
     <LinkedInButton />
+    </div>
+    <div>
+    </div>
     </body>
   );
 }
